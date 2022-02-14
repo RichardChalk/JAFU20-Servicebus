095682971efa005ec12b1edeecaa8d7819fd6f45
Richard Chalk <barmybrit@gmail.com>
2022-02-14 14:13:16 +0100

    Add project files.

---
+++ QueueReceiver/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+
+
+
+
+RecieverEntry.Run();
+
+internal class RecieverEntry
+{
+    // connection string to your Service Bus namespace
+    static string connectionString = "Endpoint=sb://richards-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SfYcU3lcVSRftV0NXeJPYl1HhR3fsWp7FrPsdISflIg=";
+
+    // name of your Service Bus queue
+    static string queueName = "MyQueue";
+
+    // the client that owns the connection and can be used to create senders and receivers
+    static ServiceBusClient client;
+
+    // the processor that reads and processes messages from the queue
+    static ServiceBusProcessor processor;
+
+
+
+    public static void Run()
+    {
+        // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when messages are being published or read
+        // regularly.
+        //
+
+        // Create the client object that will be used to create sender and receiver objects
+        client = new ServiceBusClient(connectionString);
+
+        // create a processor that we can use to process the messages
+        processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+        try
+        {
+            // add handler to process messages
+            processor.ProcessMessageAsync += MessageHandler;
+
+            // add handler to process any errors
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            // start processing 
+            processor.StartProcessingAsync();
+
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            // stop processing 
+            Console.WriteLine("\nStopping the receiver...");
+            processor.StopProcessingAsync();
+            Console.WriteLine("Stopped receiving messages");
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            processor.DisposeAsync();
+            client.DisposeAsync();
+        }
+    }
+
+    // handle received messages
+    static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        Console.WriteLine($"Received: {body}");
+
+        // complete the message. message is deleted from the queue. 
+        await args.CompleteMessageAsync(args.Message);
+    }
+
+    // handle any errors when receiving messages
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ QueueSender/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+
+
+
+
+SenderEntry.Run();
+
+internal class SenderEntry
+{
+    // connection string to your Service Bus namespace
+    static string connectionString = "Endpoint=sb://richards-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SfYcU3lcVSRftV0NXeJPYl1HhR3fsWp7FrPsdISflIg=";
+
+    // name of your Service Bus queue
+    static string queueName = "MyQueue";
+
+    // the client that owns the connection and can be used to create senders and receivers
+    static ServiceBusClient client;
+
+    // the sender used to publish messages to the queue
+    static ServiceBusSender sender;
+
+    // number of messages to be sent to the queue
+    private const int numOfMessages = 3;
+
+
+    public static void Run()
+    {
+        // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when messages are being published or read
+        // regularly.
+        //
+        // Create the clients that we'll use for sending and processing messages.
+        client = new ServiceBusClient(connectionString);
+        sender = client.CreateSender(queueName);
+
+        // create a batch 
+        using ServiceBusMessageBatch messageBatch = sender.CreateMessageBatchAsync().GetAwaiter().GetResult();
+
+        for (int i = 1; i <= numOfMessages; i++)
+        {
+            sender.SendMessageAsync(new ServiceBusMessage($"Message {i}"));
+        }
+
+        sender.DisposeAsync();
+        client.DisposeAsync();
+
+
+        Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+
+        Console.WriteLine("Press any key to end the application");
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
